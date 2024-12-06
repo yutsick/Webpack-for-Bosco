ead0d9aaecb429ca26661ab102ee8335ce92cd07
taras yurchyshyn <yts.prof@gmail.com>
2024-12-06 15:32:10 +0200

    Empty webpack template with pug

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Setup instruction
+```
+git clone https://github.com/yutsick/Webpack-for-Bosco
+npm i
+npm run dev //for start dev
+npm run build //for build
+```
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { GenerateSW } from 'workbox-webpack-plugin';
+import fastGlob from 'fast-glob';
+import process from 'node:process';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Шукаємо всі Pug-шаблони у папці src/pug/pages/
+const pages = fastGlob.sync('./src/pug/pages/*.pug');
+
+const config = {
+  entry: './src/index.js', // Якщо вам потрібен JS для основного entry
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Куди зберігати результат
+    filename: '[name].js', // Це для JS, якщо він потрібен
+  },
+  mode: isProduction ? 'production' : 'development',
+  devtool: 'source-map',
+  devServer: {
+    open: true,
+    host: 'localhost',
+    hot: true,
+    port: 8080,
+    watchFiles: ['./src/**/*'],
+  },
+  plugins: [
+    // Перевіряємо, чи правильно підключаємо HtmlWebpackPlugin для кожного шаблону
+    ...pages.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, page),  // Шлях до Pug-шаблону
+          filename: path.basename(page, '.pug') + '.html',  // Генерація HTML з ім'ям сторінки
+          // inject: false,  // Якщо потрібно вручну вставити скрипти та стилі
+        })
+    ),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', // Збираємо CSS в окремий файл
+    }),
+    isProduction && new GenerateSW(), // Для прогресивного кешування
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.pug$/, 
+        loader: 'pug-loader', 
+      },
+    ],
+  },
+};
+
+export default () => config;
